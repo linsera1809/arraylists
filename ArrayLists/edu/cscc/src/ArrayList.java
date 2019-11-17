5baede2f3e610d1426567c80dc87375d8828ace5
CORP\linser1 <bobby.linse@goodifycharities.org>
2019-11-17 15:49:56 -0500

    git init; project setup;

---
+++ ArrayLists/edu/cscc/src/ArrayList.java
@@ -0,0 +1,55 @@
+/**
+ * @author Bobby Linse
+ * Date: November 2019
+ * Instructor: David Paulzer
+ * School: Columbus State Community College
+ * Assignment: Lab #1
+ *
+ * Description: This class is dedicated to the Java II assignment which
+ * is to write a generic 'ArrayList' class similar to the one in the given
+ * lecture notes on array lists.
+ */
+
+//import statements
+
+public class ArrayList {
+
+    //Constructor
+    public ArrayList() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public void add(E value){
+
+    }
+
+    public void add(int index, E value){
+
+    }
+
+    public E get(int index){
+
+    }
+
+    public E remove(E value){
+
+    }
+
+    public E remove(int index){
+
+    }
+
+    public void clear(){
+
+    }
+
+
+}
